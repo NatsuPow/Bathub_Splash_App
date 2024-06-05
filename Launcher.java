5975527094c41e1a933b8876ea44dda3d69adc26
NatsuPow <147069989+NatsuPow@users.noreply.github.com>
2024-06-05 17:33:02 +0200

    Create Launcher.java

---
+++ Launcher.java
@@ -0,0 +1,14 @@
+package fr.ul.miage.hamouta;
+
+/**
+ * La classe de lancement de l'application.
+ * Elle utilise {@link App} pour démarrer l'application.
+ */
+public class Launcher {
+    /**
+     * Point d'entrée de l'application.
+     */
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
